d335c0360d412313bc5bb266e0ee6188ff89d782
Slaizi <bogachev_konstantin@bk.ru>
2024-03-13 05:16:43 +0300

    init

---
+++ src/main/java/ru/Bogachev/fileHosting/FileHostingApplication.java
@@ -0,0 +1,11 @@
+package ru.Bogachev.fileHosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileHostingApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(FileHostingApplication.class, args);
+    }
+}
